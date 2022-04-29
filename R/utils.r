5d782b769366f425d36151ae80ea1504730210a8
Sebastián Silva <sgsilvas96@gmail.com>
2022-04-29 16:01:47 +0200

    ms2 functiones done

--- R/utils.r
+++ R/utils.r
@@ -2,3 +2,11 @@ ppm_to_mz = function(mz, noise){
   ppm = mz / 1000000 * noise
   return(ppm)
 }
+
+calc_distances = function(x){
+  df = melt(as.matrix(dist(x@mz)), varnames=c("f1","f2"), value.name="mzDifference")
+  df = df[as.numeric(df$f1)>as.numeric(df$f2),]
+  df$f1 = x@mz[df$f1]
+  df$f2 = x@mz[df$f2]
+  return(df)
+}
